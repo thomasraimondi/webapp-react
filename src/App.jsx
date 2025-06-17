5716ea4733a8b7d1971e9eceba6a4b0bc89bc64b
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-17 13:57:08 +0200

    add movie details for axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import DefaultLayout from "./layouts/DefaultLayout.jsx";
 import Home from "./pages/Home.jsx";
 import MovieDetails from "./pages/MovieDetails.jsx";
 import Movies from "./pages/Movies.jsx";
+import { MoviesProvider } from "./contexts/moviesContext.jsx";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/movies/" element={<Movies />} />
-            <Route path="/movie/:id" element={<MovieDetails />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <MoviesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/movies/" element={<Movies />} />
+              <Route path="/movies/:id" element={<MovieDetails />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </MoviesProvider>
     </>
   );
 }
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,10 +1,34 @@
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import MovieCard from "../components/movie/MovieCard.jsx";
+import axios from "axios";
+
 export default function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState();
+
+  const getMovieById = (id) => {
+    axios
+      .get(`http://127.0.0.1:3000/movies/${id}`)
+      .then((response) => {
+        setMovie(response.data.movie);
+      })
+      .catch((error) => {
+        console.error("Error fetching movie by ID:", error);
+      });
+  };
+
+  useEffect(() => {
+    getMovieById(id);
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Movie Details Page</h1>
+      <h1 className="text-4xl font-bold mb-4">Movie {id} Details Page</h1>
       <p className="text-lg text-gray-700">
         This is the movie details page of your application.
       </p>
+      <MovieCard movie={movie} />
     </div>
   );
 }
