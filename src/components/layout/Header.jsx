5ca57b71c8856872e0c11585eb435f3337c1ee6e
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-18 13:02:44 +0200

    add logo img and change color

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,17 +2,17 @@ import { NavLink } from "react-router";
 
 export default function Header() {
   return (
-    <header className="flex gap-4 bg-blue-500 text-white p-4">
-      <h1 className="text-2xl">Movies Library</h1>
+    <header className="flex gap-4 text-black p-4">
+      <img className="h-10" src="./logo.png" alt="logo" />
       <nav className="mt-2">
         <ul className="flex space-x-4">
           <li>
-            <NavLink to="/" className="text-white hover:underline">
+            <NavLink to="/" className="hover:underline">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/movies" className="text-white hover:underline">
+            <NavLink to="/movies" className="hover:underline">
               Movies
             </NavLink>
           </li>
