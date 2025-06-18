d114ed9870357309bc12d9a5e43a0b5840551ee0
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-18 14:34:35 +0200

    move button add movie in header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,55 @@
 import { NavLink } from "react-router";
+import { useMovies } from "../../contexts/moviesContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareMinus,
+  faSquarePlus,
+} from "@fortawesome/free-regular-svg-icons";
+import { useLocation } from "react-router";
 
 export default function Header() {
+  const { addMovie, setAddMovie } = useMovies();
+  const location = useLocation();
+
+  console.log(location.pathname);
+
   return (
-    <header className="flex gap-4 text-black p-4">
-      <img className="h-10" src="./logo.png" alt="logo" />
-      <nav className="mt-2">
-        <ul className="flex space-x-4">
-          <li>
-            <NavLink to="/" className="hover:underline">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/movies" className="hover:underline">
-              Movies
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className="text-black p-4">
+      <div className="flex justify-between">
+        <nav className="mt-2">
+          <ul className="flex space-x-4">
+            <li>
+              <NavLink to="/" className="hover:underline">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/movies" className="hover:underline">
+                Movies
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        {location.pathname === "/movies" ? (
+          <div className="icon flex items-center">
+            {addMovie ? (
+              <FontAwesomeIcon
+                className="text-4xl"
+                icon={faSquareMinus}
+                onClick={() => setAddMovie(!addMovie)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                className="text-4xl"
+                icon={faSquarePlus}
+                onClick={() => setAddMovie(!addMovie)}
+              />
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </header>
   );
 }
--- src/components/movie/MovieList.jsx
+++ src/components/movie/MovieList.jsx
@@ -2,16 +2,10 @@ import MovieCard from "./MovieCard";
 import { useEffect } from "react";
 import { useMovies } from "../../contexts/moviesContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faSquarePlus,
-  faSquareMinus,
-} from "@fortawesome/free-regular-svg-icons";
 import AddMovieForm from "./addMovieForm";
-import { useState } from "react";
 
 export default function MovieList() {
-  const { movies, getMovies } = useMovies();
-  const [addMovie, setAddMovie] = useState(false);
+  const { addMovie, movies, getMovies } = useMovies();
 
   useEffect(() => {
     getMovies();
@@ -23,21 +17,6 @@ export default function MovieList() {
         <div className="flex justify-between items-center bg-gray-100 shadow-md rounded-t-lg">
           <div className="p-4 flex items-center">
             <h2 className="text-2xl font-bold">Movies List</h2>
-          </div>
-          <div className="icon p-4 flex items-center">
-            {addMovie ? (
-              <FontAwesomeIcon
-                className="text-4xl"
-                icon={faSquareMinus}
-                onClick={() => setAddMovie(!addMovie)}
-              />
-            ) : (
-              <FontAwesomeIcon
-                className="text-4xl"
-                icon={faSquarePlus}
-                onClick={() => setAddMovie(!addMovie)}
-              />
-            )}
           </div>
         </div>
         {addMovie && (
