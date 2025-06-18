0499dee60011a5ac01b87cafe27d00b9b371e282
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-18 16:15:17 +0200

    add confimation of delete reviews

--- src/components/review/ReviewCard.jsx
+++ src/components/review/ReviewCard.jsx
@@ -1,18 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
+import DeleteConfirm from "../ui/DeleteConfirm";
+
 export default function ReviewCard({ review }) {
+  const [confirmDelete, setConfirmDelete] = useState(false);
+
   return (
-    <div key={review.id} className="border-b border-gray-200 py-4 w-full">
-      <div className="flex mb-3">
-        <div className="min-w-10 h-10 text-lg/10 text-center rounded-full bg-green-500 mx-1">
-          {review.name[0]}
+    <>
+      <div key={review.id} className="border-b border-gray-200 py-4 w-full">
+        <div className="flex gap-3 mb-3">
+          <div className="min-w-10 h-10 text-lg/10 text-center rounded-full bg-green-500 mx-1">
+            {review.name[0]}
+          </div>
+          <p className="text-gray-800 grow">
+            {review.text ? review.text : "No text available."}
+          </p>
+          <p className="text-gray-800">Vote: {review.vote}</p>
+          <FontAwesomeIcon
+            className="text-gray-800 text-xl text-red-600 cursor-pointer"
+            icon={faTrashCan}
+            onClick={() => setConfirmDelete(true)}
+          />
         </div>
-        <p className="text-gray-800 grow">
-          {review.text ? review.text : "No text available."}
+        <p className="text-sm text-gray-500">
+          - {review.name} on {new Date(review.created_at).toLocaleDateString()}
         </p>
-        <p className="text-gray-800">Vote: {review.vote}</p>
       </div>
-      <p className="text-sm text-gray-500">
-        - {review.name} on {new Date(review.created_at).toLocaleDateString()}
-      </p>
-    </div>
+      {confirmDelete && (
+        <DeleteConfirm
+          entity="reviews"
+          id={review.id}
+          cancel={setConfirmDelete}
+        />
+      )}
+    </>
   );
 }
---
+++ src/components/ui/DeleteConfirm.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function DeleteConfirm({ entity, id, cancel }) {
+  useEffect(() => {
+    console.log(entity, id, cancel);
+  }, []);
+
+  const handleSubmit = () => {
+    axios.delete(`http://127.0.0.1:3000/${entity}/${id}`).then((response) => {
+      calcel(false);
+    });
+  };
+
+  return (
+    <div className="flex justify-center items-center fixed top-0 left-0 bg-black/70 h-screen w-full">
+      <div className="bg-white text-black w-1/4 rounded-2xl p-4">
+        Confimr Delete?
+        <form onSubmit={handleSubmit}>
+          <button type="submit" className="rounded-lg bg-green-300 p-5">
+            si
+          </button>
+          <button
+            type="button"
+            className="rounded-lg bg-red-300 p-5"
+            onClick={() => cancel(false)}
+          >
+            no
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
