228334fec6f464421cd66dae35631f7a46631139
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-18 15:18:44 +0200

    change color text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <div className="flex items-center justify-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+        <h1 className="text-6xl font-bold mb-4 text-white">
+          Welcome to the Home Page
+        </h1>
       </div>
     </div>
   );
