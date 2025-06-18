e33b5815850202a47c7a3b06457cd8ece6579066
thomasraimondi <thomas.raimondi14@gmail.com>
2025-06-18 13:03:19 +0200

    edit movie list

--- src/components/movie/MovieList.jsx
+++ src/components/movie/MovieList.jsx
@@ -1,26 +1,58 @@
 import MovieCard from "./MovieCard";
 import { useEffect } from "react";
 import { useMovies } from "../../contexts/moviesContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquarePlus,
+  faSquareMinus,
+} from "@fortawesome/free-regular-svg-icons";
+import AddMovieForm from "./addMovieForm";
+import { useState } from "react";
 
 export default function MovieList() {
   const { movies, getMovies } = useMovies();
+  const [addMovie, setAddMovie] = useState(false);
 
   useEffect(() => {
     getMovies();
   }, []);
 
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Movies List</h2>
-      <p className="text-gray-700">This is the Movies page.</p>
-      <p className="text-gray-700">You can add your movies here.</p>
-      <div className="flex gap-4 flex-wrap">
+    <>
+      <div className="flex flex-col mb-10">
+        <div className="flex justify-between items-center bg-gray-100 shadow-md rounded-t-lg">
+          <div className="p-4 flex items-center">
+            <h2 className="text-2xl font-bold">Movies List</h2>
+          </div>
+          <div className="icon p-4 flex items-center">
+            {addMovie ? (
+              <FontAwesomeIcon
+                className="text-4xl"
+                icon={faSquareMinus}
+                onClick={() => setAddMovie(!addMovie)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                className="text-4xl"
+                icon={faSquarePlus}
+                onClick={() => setAddMovie(!addMovie)}
+              />
+            )}
+          </div>
+        </div>
+        {addMovie && (
+          <div className="bg-white p-6 shadow-md rounded-b-lg">
+            <AddMovieForm />
+          </div>
+        )}
+      </div>
+      <div className="flex justify-center gap-8 flex-wrap">
         {movies ? (
           movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
         ) : (
           <div>No posts found</div>
         )}
       </div>
-    </div>
+    </>
   );
 }
